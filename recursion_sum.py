389dae3c7f8e79245d2aaeec4bad7a088b376966
korzia <Qwer2345>
2022-04-07 15:14:48 +0300

    1

---
+++ recursion_sum.py
@@ -0,0 +1,28 @@
+import random
+import copy
+
+def recursion_sum(arr):
+    if len(arr) == 0:
+        return 0
+    elif len(arr) == 1:
+        return arr[0]
+    else:
+        arr_b = copy.copy(arr)
+        arr_b.pop(0)
+        return arr[0] + recursion_sum(arr_b)
+
+def non_rec_sum(arr):
+    summed_arr = 0
+    for i in range(0, len(arr)):
+        summed_arr += arr[i]
+    return summed_arr
+
+
+initial = []
+for i in range(0, 5):
+    initial.append(random.randint(0, 5))
+print('init: %s' % initial)
+correct_mod = non_rec_sum(initial)
+print('correct sum is %s' % correct_mod)
+modified = recursion_sum(initial)
+print("modified is %s" % modified)
