9d5d980dd6e7a469324e1a88fe29d3c0680e3f19
korzia <Qwer2345>
2022-04-08 19:48:20 +0300

    1

--- hash_table.py
+++ hash_table.py
@@ -1,14 +1,9 @@
 from collections import deque
 import random
-
-def generate_random_int_string(length):
-    string = ''
-    for i in range(0, length):
-        string += str(random.randint(0,9))
-    return string
+import string
 
 def check_if_this_what_i_search(name):
-    return name[-2:] == '00'
+    return name[-2:] == 'gy'
 
 def search_queue(grapp, name):
     search_q = deque()
@@ -25,16 +20,31 @@ def search_queue(grapp, name):
                 search_q += grapp[current]
                 searched.append(current)
     return False
+def generate_random_string(length):
+    letters = string.ascii_lowercase
+    stroing = ''
+    for i in range(0, length):
+        stroing += random.choice(letters)
+    return stroing
 
 
+def generate_graph():
+
+    graph = {}
+    graph[generate_random_string(3)] = [generate_random_string(4), generate_random_string(4), generate_random_string(4)]
+    print(graph)
+    print(list(graph)[0])
+    first_in_graph = list(graph)[0]
+    fill_que = deque()
+    fill_que += graph[first_in_graph]
+    while fill_que:
+        current = fill_que.popleft()
+        graph[current] = [generate_random_string(5), generate_random_string(5), generate_random_string(5)]
+    print(graph)
+    return graph
 
 
-graph = {}
-graph['firsts'] = ['second1', 'second2', 'second3']
-graph['second1'] = [generate_random_int_string(4), generate_random_int_string(4), generate_random_int_string(4)]
-graph['second2'] = [generate_random_int_string(4), generate_random_int_string(4), generate_random_int_string(4)]
-graph['second3'] = [generate_random_int_string(4), generate_random_int_string(4), generate_random_int_string(4)]
-
-print(graph)
-search_queue(graph, 'firsts')
-
+graph2 = generate_graph()
+print(graph2)
+first_in_graph2 = list(graph2)[0]
+search_queue(graph2, first_in_graph2)
