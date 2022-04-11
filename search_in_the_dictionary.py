78e46c343b91e350dec5bbc575ed4ec11ec60fee
korzia <Qwer2345>
2022-04-11 12:32:47 +0300

    1

--- search_in_the_dictionary.py
+++ search_in_the_dictionary.py
@@ -2,18 +2,17 @@ from collections import deque
 import random
 import string
 
-def check_if_this_what_i_search(name):
-    return name[-2:] == 'gy'
+def check_if_this_what_i_search(name, piece):
+    return name[-2:] == piece
 
-def search_queue(grapp, name):
+def search_queue(grapp, name, piece):
     search_q = deque()
     search_q += grapp[name]
-    print(search_q)
     searched = []
     while search_q:
         current = search_q.popleft()
         if not current in searched:
-            if check_if_this_what_i_search(current):
+            if check_if_this_what_i_search(current, piece):
                 print('i found %s' % current)
                 return True
             else:
@@ -33,7 +32,7 @@ def generate_graph():
     graph = {}
     graph[generate_random_string(3)] = [generate_random_string(4), generate_random_string(4), generate_random_string(4)]
     print(graph)
-    print(list(graph)[0])
+    print('first item is %s' % list(graph)[0])
     first_in_graph = list(graph)[0]
     fill_que = deque()
     fill_que += graph[first_in_graph]
@@ -45,14 +44,18 @@ def generate_graph():
     empty_fill_que += graph[first_in_graph]
     while empty_fill_que:
         ecurrent = empty_fill_que.popleft()
-        empty_array = []
         if ecurrent not in graph:
-            graph[ecurrent] = empty_array
+            graph[ecurrent] = []
+        else:
+            empty_fill_que += graph[ecurrent]
 
     return graph
 
 
 graph2 = generate_graph()
-print(graph2)
+print('initial graph is %s' % graph2)
+item_to_search = generate_random_string(2)
 first_in_graph2 = list(graph2)[0]
-search_queue(graph2, first_in_graph2)
+print('i am searching for -%s in graph' % item_to_search)
+isFound = search_queue(graph2, first_in_graph2, item_to_search)
+print('did i find item? %s' % isFound)
