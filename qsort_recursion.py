1facec36eb27d61febe7f098c330d362d6d77f27
korzia <Qwer2345>
2022-04-21 17:47:18 +0300

    1

---
+++ qsort_recursion.py
@@ -0,0 +1,13 @@
+import random
+import copy
+
+def quick_sort(arr):
+    if len(arr) < 2
+        return arr
+
+
+
+initial = []
+for i in range(0, 5):
+    initial.append(random.randint(0, 5))
+print('init: %s' % initial)
