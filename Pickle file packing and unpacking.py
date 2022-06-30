c19b60b067234ff521ad905cc6d11922e6c6db51
korzia <Qwer2345>
2022-06-30 13:53:51 +0300

    added sample pickle pack/unpack

---
+++ Pickle file packing and unpacking.py
@@ -0,0 +1,11 @@
+import random
+import string
+import pickle
+with open('data.pkl', 'w+b') as data:
+    arr1 = [random.randrange(1,100) for _ in range(10)]
+    arr2 = [random.choice(string.ascii_lowercase) for _ in range(10)]
+    dict1 = dict(zip(arr1, arr2))
+    pickle.dump(dict1, data)
+with open('data.pkl', 'rb') as data:
+    readed = pickle.load(data)
+    print(readed)
